085b67659c14accfd9974a0405ff52bfbf3f86cd
Oran9eUtan <oran9eutan@gmail.com>
2022-01-18 21:23:41 +0100

    feat: Basic list of hats

---
+++ src/routes/hats/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { hatsRegistry } from '../../stores/hats';
+</script>
+
+<svelte:head>
+	<title>Hats</title>
+</svelte:head>
+
+{#each Object.entries($hatsRegistry) as [category, hats]}
+	<h2>{category.trim().replace(/^\w/, (c) => c.toUpperCase())}</h2>
+	{#each Object.entries(hats) as [id, hat]}
+		<img src={`images/hats/icons/${id}.png`} alt="" />
+	{/each}
+{/each}
---
+++ src/stores/hats.ts
@@ -0,0 +1,21 @@
+import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
+import yaml from 'js-yaml';
+
+interface Hat {
+	cmd: number;
+	lore?: Array<string>;
+}
+type HatsRegistry = Record<string, Record<string, Hat>>;
+
+export const hatsRegistry: Writable<HatsRegistry> = writable({});
+
+const fetchHatsRegistry = async () => {
+	const url = 'https://raw.githubusercontent.com/OrangeUtan/Hats/main/hats/registry/hats.yml';
+	const res = await fetch(url);
+	const blob = await res.blob();
+	const text = await blob.text();
+	hatsRegistry.set(yaml.load(text));
+};
+
+fetchHatsRegistry();
