3e21d81aa89c114e317e5c23384d194d504101fc
Oran9eUtan <oran9eutan@gmail.com>
2022-01-23 00:34:26 +0100

    feat: Caching hats_by_category fetch

--- src/lib/stores/hats.ts
+++ src/lib/stores/hats.ts
@@ -1,5 +1,6 @@
-import { writable } from 'svelte/store';
+import { derived, get, writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
+import { isEmptyObject } from '$lib/utils';
 
 interface HatJson {
 	type: string;
@@ -17,30 +18,54 @@ export interface Hat {
 }
 
 export const categories: Writable<Record<string, Hat[]>> = writable({});
+export const hats = derived(
+	categories,
+	($categories) => {
+		return Object.values($categories).reduce((prev: Record<string, Hat>, curr) => {
+			return { ...prev, ...Object.fromEntries(curr.map((hat) => [hat.type, hat])) };
+		}, {});
+	},
+	{}
+);
 
-const fetchCategories = async () => {
+export async function fetchCategories(force = false): Promise<Record<string, Hat[]>> {
+	const currentVal = get(categories);
+	if (!force && !isEmptyObject(currentVal)) {
+		return currentVal;
+	}
+
 	const url = 'https://orangeutan.github.io/Hats/api/hats_by_category.json';
 	const res = await fetch(url);
 	const categoriesJson: Record<string, HatJson[]> = await res.json();
-
-	categories.set(
-		Object.fromEntries(
-			Object.entries(categoriesJson).map(([category, hats]) => {
-				return [
-					category,
-					hats.map((hatJson) => {
-						return {
-							type: hatJson.type,
-							cmd: hatJson.cmd,
-							name: `item.hats.hat.${hatJson.type}.name`,
-							lore: hatJson.lore,
-							additional_nbt: hatJson.additional_nbt
-						} as Hat;
-					})
-				];
-			})
-		)
+	const data = Object.fromEntries(
+		Object.entries(categoriesJson).map(([category, hats]) => {
+			return [
+				category,
+				hats.map((hatJson) => {
+					return {
+						type: hatJson.type,
+						cmd: hatJson.cmd,
+						name: `item.hats.hat.${hatJson.type}.name`,
+						lore: hatJson.lore,
+						additional_nbt: hatJson.additional_nbt
+					} as Hat;
+				})
+			];
+		})
 	);
-};
+
+	categories.set(data);
+	return data;
+}
+
+export async function fetchHats(force = false): Promise<Record<string, Hat>> {
+	const currentVal = get(hats);
+	if (!force && !isEmptyObject(currentVal)) {
+		return currentVal;
+	}
+
+	await fetchCategories((force = true));
+	return get(hats);
+}
 
 fetchCategories();
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function sleep(delay) {
 	return new Promise((resolve) => setTimeout(resolve, delay));
 }
+
+export function isEmptyObject(obj) {
+	return Object.keys(obj).length === 0;
+}
