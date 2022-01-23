ffd70c5544d09b2cb537c5fdc19de3fcb91a12d7
Oran9eUtan <oran9eutan@gmail.com>
2022-01-23 10:46:05 +0100

    feat: Added shadow to navbar

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,6 +1,6 @@
-<nav class="flex justify-start relative items-center pt-2 pb-2 bg-gray-100">
+<nav class="flex justify-start relative items-center pt-2 pb-2 bg-gray-100 shadow-md">
 	<div class="w-full flex items-center pr-3 pl-3">
-		<a href="/" class="text-xl pt-1 pb-1">
+		<a href="/" class="text-xl">
 			<img class="inline-block align-text-top" src="/i/favicon.png" alt="" width="30" height="30" />
 			Hats
 		</a>
